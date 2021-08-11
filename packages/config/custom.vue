37283e3c1145e4dec446aa690174a4281a65bb26
wangxiang <1827945911@qq.com>
2021-08-11 16:24:05 +0800

    dev version 1.6.8

--- packages/config/custom.vue
+++ packages/config/custom.vue
@@ -3,10 +3,10 @@
     <el-form-item label="字段标识">
       <el-input v-model="data.prop" clearable/>
     </el-form-item>
-    <el-form-item v-loquat-has-perm="[originData, 'label']" label="标题">
+    <el-form-item label="标题">
       <el-input v-model="data.label" clearable/>
     </el-form-item>
-    <el-form-item v-loquat-has-perm="[originData, 'labelWidth']" label="标签宽度">
+    <el-form-item label="标签宽度">
       <el-input v-model.number="data.labelWidth" type="number" placeholder="请输入标签宽度"/>
     </el-form-item>
     <el-form-item label="自定义属性">
@@ -40,35 +40,37 @@ export default {
   },
   data () {
     return {
-      params: beautifier(this.data.params) || '{}',
-      events: beautifier(this.data.events) || '{}',
-      originData: this.$loquat.deepClone(this.data)
+      params: beautifier(this.data.params) || '',
+      events: beautifier(this.data.events) || ''
     }
   },
   computed: {
     style () {
       return this.data.style || {}
+    },
+    originData () {
+      return this.$loquat.deepClone(this.data)
     }
   },
   watch: {
     'data.params' (val) {
-      this.params = beautifier(val) || '{}'
+      this.params = beautifier(val) || ''
     },
     'data.events' (val) {
-      this.events = beautifier(val) || '{}'
+      this.events = beautifier(val) || ''
     },
     params (val) {
       try {
         this.data.params = eval('(' + val + ')')
       } catch (e) {
-        // console.error(e)
+        console.warn('自定义参数数据解析失败,请调整参数后重试')
       }
     },
     events (val) {
       try {
         this.data.events = eval('(' + val + ')')
       } catch (e) {
-        // console.error(e)
+        console.warn('自定义事件数据解析失败,请调整参数后重试')
       }
     }
   }
--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,6 @@ export default {
             title: '分割线',
             component: 'el-divider',
             icon: 'iconfont icon-divider',
-            label: '',
             labelWidth: 0,
             params: {
               html: '<h3 style="color:red">分割线标题</h3>',
@@ -30,7 +29,6 @@ export default {
             title: '警告',
             component: 'el-alert',
             icon: 'el-icon-warning',
-            label: '',
             labelWidth: 0,
             params: {
               title: '警告警告警告警告',
