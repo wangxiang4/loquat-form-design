5972ca360ffc23a4d31ff4ba7afd85b19611322e
wangxiang <1827945911@qq.com>
2021-09-29 12:16:27 +0800

    解决设计器配置转换设计器预览配置存在布局字段存在的递归问题

--- packages/utils/dataFormat.js
+++ packages/utils/dataFormat.js
@@ -128,112 +128,139 @@ export function designTransformPreview (_this) {
   const data = deepClone(_this.option)
   const eventScript = data.eventScript
   const autoDataSource = data.dataSource && data.dataSource.filter(item => item.auto)
-  for (let i = 0; i < data.column.length; ++i) {
-    const col = data.column[i]
-    // 处理动作转换数据
-    if (!validateNull(col.events)) {
-      for (const key in col.events) {
-        if (typeof col.events[key] !== 'string') continue
-        if (col.events[key]) {
-          const event = eventScript && eventScript.find(item => item.name === col.events[key])
-          col.events[key] = new Function(event?.func)
-        } else delete col.events[key]
-      }
-    }
-    // 处理上传数据
-    if (col.type === 'upload') {
-      // 转换请求头部与请求参数数据格式
-      col.headers = col.headers && Object(...col.headers.map(({ key, value }) => ({ [key]: value })))
-      col.data = col.data && Object(...col.data.map(({ key, value }) => ({ [key]: value })))
-      // 处理动作数据
-      for (const key in col.events) col[key] = col.events[key]
-      delete col.events
-    }
-    // 处理远端请求数据转换
-    if (SELECT_LIST.includes(col.type) || col.type === 'upload') {
-      if (col.static) _this.$set(_this.DIC, col.prop, col.dicData)
-      if (!col.static) {
-        const dataSource = autoDataSource && autoDataSource.find(item => item.key === col.remoteDataSource)
-        // 提取请求参数
-        const param = (({ url, method, headers, params }) => {
-          return { url, method, headers, params }
-        })(dataSource || {})
-        // 远端请求显示标签不能手动控制,设置显示
-        if (hasOwnProperty(col, 'showLabel')) col.showLabel = true
-        switch (col.remoteType) {
-          case 'option' :
-            if (_this.$loquat.remoteOption[col.remoteOption]) _this.$set(_this.DIC, col.prop, _this.$loquat.remoteOption[col.remoteOption])
-            break
-          case 'func' :
-            if (_this.$loquat.remoteFunc[col.remoteFunc]) _this.$set(_this.DIC, col.prop, _this.$loquat.remoteFunc[col.remoteFunc]())
-            break
-          case 'datasource' :
-            if (!dataSource) break
-            // 是否使用第三方Axios请求
-            if (dataSource.thirdPartyAxios) {
-              !validateNull(_this.$loquat.axios) ? _this.$loquat.axios(param).then(res => {
-                try {
-                  const execute = new Function('res', dataSource.responseFunc)(res)
-                  _this.$set(_this.DIC, col.prop, responseDataAccept(execute, col.type))
-                } catch (e) {
-                  console.error(e)
-                }
-              }).catch(error => {
-                try {
-                  const execute = new Function('error', dataSource.errorFunc)(error)
-                  console.error(execute)
-                } catch (e) {
-                  console.error(e)
-                }
-              }) : packages.logs('thirdPartyAxios')
-            } else {
-              request.interceptors.request.empty()
-              dataSource.method !== 'GET' && request.interceptors.request.use(config => {
-                return new Function('config', dataSource.requestFunc)(config)
-              }, undefined)
-              request(param).then(res => {
-                try {
-                  const execute = new Function('res', dataSource.responseFunc)(res)
-                  _this.$set(_this.DIC, col.prop, responseDataAccept(execute, col.type))
-                } catch (e) {
-                  console.error(e)
-                }
-              }).catch(error => {
-                try {
-                  const execute = new Function('error', dataSource.errorFunc)(error)
-                  console.error(execute)
-                } catch (e) {
-                  console.error(e)
-                }
-              })
-            }
-            break
-        }
-      }
-      delete col.static
-      delete col.dicData
-      delete col.remoteDataSource
-      delete col.remoteType
-      delete col.remoteFunc
-      delete col.remoteOption
-    }
-    // 校验规则处理
-    if (col.validateConfig) {
-      const rules = []
-      if (hasOwnProperty(col, '_type')) col._type = ''
-      col.validateConfig.required && rules.push({ required: true, message: col.validateConfig.requiredMessage || `${col.label}必须填写` })
-      if (col.validateConfig.type) {
-        rules.push({ type: col.validateConfig.typeFormat, message: col.validateConfig.typeMessage || `${col.label}格式不正确` })
-        if (['number', 'integer', 'float'].includes(col.validateConfig.typeFormat) && hasOwnProperty(col, '_type')) col._type = 'number'
-      }
-      col.validateConfig.pattern && rules.push({ pattern: col.validateConfig.patternFormat, message: col.validateConfig.patternMessage || `${col.label}格式不匹配` })
-      delete col.validateConfig
-      col.rules = rules
-    }
-  }
+  handleDeepDesignTransformPreview(_this, data.column, {
+    eventScript,
+    autoDataSource
+  })
   delete data.dataSource
   delete data.eventScript
   return data
+}
+
+/** 处理设计器配置转换设计器预览配置深度递归 **/
+function handleDeepDesignTransformPreview (_this, column, ops = {}) {
+  // 设置参数配置,为了后面好扩展单独提出来
+  const options = {
+    eventScript: ops.eventScript || [],
+    autoDataSource: ops.autoDataSource || []
+  }
+  for (let i = 0; i < column.length; ++i) {
+    const col = column[i]
+    switch (col.type) {
+      // 珊瑚布局数据处理
+      case 'coralLayoutRow':
+        for (let colIndex = 0; colIndex < col.cols.length; ++colIndex) {
+          const coralCol = col.cols[colIndex]
+          handleDeepDesignTransformPreview(_this, coralCol.list, {
+            eventScript: ops.eventScript,
+            autoDataSource: ops.autoDataSource
+          })
+        }
+        break
+      // 插件数据处理
+      default:
+        // 处理动作转换数据
+        if (!validateNull(col.events)) {
+          for (const key in col.events) {
+            if (typeof col.events[key] !== 'string') continue
+            if (col.events[key]) {
+              const event = options.eventScript.find(item => item.name === col.events[key])
+              col.events[key] = new Function(event?.func)
+            } else delete col.events[key]
+          }
+        }
+        // 处理上传数据
+        if (col.type === 'upload') {
+          // 转换请求头部与请求参数数据格式
+          col.headers = col.headers && Object(...col.headers.map(({ key, value }) => ({ [key]: value })))
+          col.data = col.data && Object(...col.data.map(({ key, value }) => ({ [key]: value })))
+          // 处理动作数据
+          for (const key in col.events) col[key] = col.events[key]
+          delete col.events
+        }
+        // 处理远端请求数据转换
+        if (SELECT_LIST.includes(col.type) || col.type === 'upload') {
+          if (col.static) _this.$set(_this.DIC, col.prop, col.dicData)
+          if (!col.static) {
+            const dataSource = options.autoDataSource.find(item => item.key === col.remoteDataSource)
+            // 提取请求参数
+            const param = (({ url, method, headers, params }) => {
+              return { url, method, headers, params }
+            })(dataSource || {})
+            // 远端请求显示标签不能手动控制,设置显示
+            if (hasOwnProperty(col, 'showLabel')) col.showLabel = true
+            switch (col.remoteType) {
+              case 'option' :
+                if (_this.$loquat.remoteOption[col.remoteOption]) _this.$set(_this.DIC, col.prop, _this.$loquat.remoteOption[col.remoteOption])
+                break
+              case 'func' :
+                if (_this.$loquat.remoteFunc[col.remoteFunc]) _this.$set(_this.DIC, col.prop, _this.$loquat.remoteFunc[col.remoteFunc]())
+                break
+              case 'datasource' :
+                if (!dataSource) break
+                // 是否使用第三方Axios请求
+                if (dataSource.thirdPartyAxios) {
+                  !validateNull(_this.$loquat.axios) ? _this.$loquat.axios(param).then(res => {
+                    try {
+                      const execute = new Function('res', dataSource.responseFunc)(res)
+                      _this.$set(_this.DIC, col.prop, responseDataAccept(execute, col.type))
+                    } catch (e) {
+                      console.error(e)
+                    }
+                  }).catch(error => {
+                    try {
+                      const execute = new Function('error', dataSource.errorFunc)(error)
+                      console.error(execute)
+                    } catch (e) {
+                      console.error(e)
+                    }
+                  }) : packages.logs('thirdPartyAxios')
+                } else {
+                  request.interceptors.request.empty()
+                  dataSource.method !== 'GET' && request.interceptors.request.use(config => {
+                    return new Function('config', dataSource.requestFunc)(config)
+                  }, undefined)
+                  request(param).then(res => {
+                    try {
+                      const execute = new Function('res', dataSource.responseFunc)(res)
+                      _this.$set(_this.DIC, col.prop, responseDataAccept(execute, col.type))
+                    } catch (e) {
+                      console.error(e)
+                    }
+                  }).catch(error => {
+                    try {
+                      const execute = new Function('error', dataSource.errorFunc)(error)
+                      console.error(execute)
+                    } catch (e) {
+                      console.error(e)
+                    }
+                  })
+                }
+                break
+            }
+          }
+          delete col.static
+          delete col.dicData
+          delete col.remoteDataSource
+          delete col.remoteType
+          delete col.remoteFunc
+          delete col.remoteOption
+        }
+        // 校验规则处理
+        if (col.validateConfig) {
+          const rules = []
+          if (hasOwnProperty(col, '_type')) col._type = ''
+          col.validateConfig.required && rules.push({ required: true, message: col.validateConfig.requiredMessage || `${col.label}必须填写` })
+          if (col.validateConfig.type) {
+            rules.push({ type: col.validateConfig.typeFormat, message: col.validateConfig.typeMessage || `${col.label}格式不正确` })
+            if (['number', 'integer', 'float'].includes(col.validateConfig.typeFormat) && hasOwnProperty(col, '_type')) col._type = 'number'
+          }
+          col.validateConfig.pattern && rules.push({ pattern: col.validateConfig.patternFormat, message: col.validateConfig.patternMessage || `${col.label}格式不匹配` })
+          delete col.validateConfig
+          col.rules = rules
+        }
+    }
+  }
 }
 
 /** 字段数据转换为插件数据 **/
